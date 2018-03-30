03928c2bdbf1eaa3939a1426712b5cff0ec2bcda
dan-watson-ucb <dan.watson@ischool.berkeley.edu>
2018-03-30 08:18:51 -0400

    district/sub-district menus added

--- dash_app/water2.py
+++ dash_app/water2.py
@@ -8,14 +8,18 @@ import psycopg2
 import pandas as pd
 from dash.dependencies import Output, Event, Input, State
 
+## Connect and pull initial df
 conn = psycopg2.connect("dbname='water_db' user='dan' host='postgres-instance2.clhlqrsuvowr.us-east-1.rds.amazonaws.com' password='berkeley'")
-query = "SELECT lat_deg, lon_deg, status_id, district, country_name, fuzzy_water_source, fuzzy_water_tech, management from final_all WHERE country_name = 'Sierra Leone'"
+query = "SELECT lat_deg, lon_deg, status_id, district, sub_district, country_name, fuzzy_water_source, fuzzy_water_tech, management from final_all WHERE country_name = 'Sierra Leone'"
 df_init = pd.read_sql_query(query, conn)
 mapbox_access_token = 'pk.eyJ1IjoiZHdhdHNvbjgyOCIsImEiOiJjamVycHp0b3cxY2dyMnhsdGc4eHBkcW85In0.uGPxMK4_u-nAs_J74yw70A'
 
-q2 = "SELECT distinct(country_name) from final_all"
+## query for countries, districts, subdistricts
+q2 = "SELECT country_name, district, sub_district from final_all"
 df2 = pd.read_sql_query(q2, conn)
 
+
+## insertion point for css and js
 external_css = [
     'https://codepen.io/chriddp/pen/bWLwgp.css',
     'https://api.tiles.mapbox.com/mapbox-gl-js/v0.44.1/mapbox-gl.css'
@@ -30,6 +34,7 @@ external_js = [
 
 app = dash.Dash()
 app.title = "water site"
+##JS and CSS app integration
 for js in external_js:
     app.scripts.append_script({'external_url': js})
 
@@ -47,24 +52,23 @@ app.layout = html.Div([
         dcc.Dropdown(id = 'country-select',
             options=[
              {'label': i, 'value': i} for i in df2.country_name.sort_values().unique()],
-             value = df2.country_name.sort_values().unique()[0], 
+             #value = df2.country_name.sort_values().unique()[0], 
              placeholder = "Select a Country"       
         ),
         dcc.Dropdown(id = 'district-select',
             options = [
             #This will have to be updated based on country- need master table
             {'label': i, 'value': i} for i in df_init.district.sort_values().unique()],
-            value = df_init['district'].sort_values().unique()[0:2],
+            #value = df_init['district'].sort_values().unique()[0:2],
             placeholder = 'Select a District',
             multi= True
         ),
-        ##We can add sub-district here, but need to add another layer of callback
-        # dcc.Dropdown(id = 'sub-district-select',
-        #     options = [
-        #     #This will have to be updated based on country- need master table
-        #     {'label': i, 'value': i} for i in df_init.sub_district.sort_values().unique()],
-        #     placeholder = 'Select a Sub-District'
-        # ),
+        dcc.Dropdown(id = 'sub-district-select',
+            options = [
+            {'label': i, 'value': i} for i in df_init.sub_district.sort_values().unique()],
+            placeholder = 'Select a Sub-District',
+            multi = True
+        ),
         dcc.Dropdown(id = 'status-select',
             options = [
             {'label': 'Working' , 'value': 'yes'},
@@ -99,8 +103,6 @@ app.layout = html.Div([
              placeholder = "Select Water Point Management",
              multi = True       
         ),
-
-
         html.Button('submit', id= 'submit-button'),
         dcc.Graph(id = "output-graph",
         figure ={"data": [
@@ -123,12 +125,12 @@ app.layout = html.Div([
             "mapbox": {
                 "accesstoken": mapbox_access_token,
                 "center": {
-                    "lat": df_init.lat_deg.mean(),
-                    "lon": df_init.lon_deg.mean()
+                    "lat": (df_init.lat_deg.max()- df_init.lat_deg.min())/2,
+                    "lon": (df_init.lon_deg.max()- df_init.lon_deg.min())/2
                 },
                 "cluster": True,
                 "pitch": 0,
-                "zoom": 3,
+                "zoom": 7,
                 "style": "outdoors"
             }
         }
@@ -139,21 +141,23 @@ app.layout = html.Div([
 
 
 
-
+## Update graph with query from filters above
 @app.callback(Output('output-graph', 'figure'), [Input('submit-button', 'n_clicks')], state= [State('country-select', 'value'),
- State('status-select', 'value'), State('district-select', 'value'), State('watersource-select', 'value'), State('watertech-select', 'value'),
+ State('status-select', 'value'), State('district-select', 'value'), State('sub-district-select', 'value'), State('watersource-select', 'value'), State('watertech-select', 'value'),
  State('management-select', 'value')])
-def run_query(n_clicks, country, status, district, fuzzy_water_source, fuzzy_water_tech, management):
+def run_query(n_clicks, country, status, district, sub_district, fuzzy_water_source, fuzzy_water_tech, management):
     conn = psycopg2.connect("dbname='water_db' user='dan' host='postgres-instance2.clhlqrsuvowr.us-east-1.rds.amazonaws.com' password='berkeley'")
     query = "SELECT lat_deg, lon_deg, status_id from final_all WHERE \
     country_name = '{}' and \
     status_id in ({}) and \
     district in ({}) and \
+    sub_district in ({}) and \
     fuzzy_water_source in ({}) and \
     fuzzy_water_tech in ({}) and \
     management in ({})".format(country, \
         ', '.join("'" + i + "'" for i in status),\
         ', '.join("'" + i + "'" for i in district), \
+        ', '.join("'" + i + "'" for i in sub_district), \
         ', '.join("'" + i + "'" for i in fuzzy_water_source),\
         ', '.join("'" + i + "'" for i in fuzzy_water_tech),\
         ', '.join("'" + i + "'" for i in management))
@@ -191,6 +195,21 @@ def run_query(n_clicks, country, status, district, fuzzy_water_source, fuzzy_wat
     }
     return figure
 
+##update districts menu
+@app.callback(Output('district-select', 'options'), [Input('country-select', 'value')])
+def update_district(country_name):
+    return[{'label': i, 'value': i} for i in df2.district[df2.country_name== country_name].sort_values().unique()]
+
+##update sub-districts menu- tried to do this like the above example but got errors- think it becomes somewhat circular?
+@app.callback(Output('sub-district-select', 'options'), [Input('district-select', 'value'), Input('country-select', 'value')])
+def update_subdistrict(district_name, country):
+    conn = psycopg2.connect("dbname='water_db' user='dan' host='postgres-instance2.clhlqrsuvowr.us-east-1.rds.amazonaws.com' password='berkeley'")
+    query = "SELECT distinct(sub_district) from final_all \
+    WHERE country_name = '{}' and \
+    district in ({})".format(country, \
+    ', '.join("'" + i + "'" for i in district_name))
+    df_subdistricts = pd.read_sql_query(query, conn)
+    return [{'label': i, 'value': i} for i in df_subdistricts.sub_district.sort_values().unique()]   
 
 
 
